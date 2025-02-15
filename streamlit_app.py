34d053a5d46e15871a2155269ca872b7b9c014cc
Sukruth S <100787053+I-Sukruth@users.noreply.github.com>
2025-02-15 11:16:59 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,21 +3,19 @@ from keras.models import load_model
 from keras.preprocessing import image
 import numpy as np
 import gdown
-from PIL import Image, ImageOps  # Added ImageOps to handle grayscale images
+from PIL import Image, ImageOps
 
-# Function to download the model from Google Drive and load it
+# Function to download the model from Google Drive
 @st.cache_resource
 def load_model_from_drive():
     url = 'https://drive.google.com/uc?id=1ucpbyJY61OnCSRKbrj4FgYBSXX8FSONy'
     output = 'Eco_Bin.h5'
     
-    # Download the model file from Google Drive
     gdown.download(url, output, quiet=False)
     
     # Load the model
     return load_model(output)
 
-# Load the model
 model = load_model_from_drive()
 
 # Define the class labels
@@ -28,17 +26,17 @@ class_labels = [
 
 # Function to make predictions
 def predict_image(img):
-    # Convert image to RGB if it's not already in RGB mode
+    # Convert image to RGB if it's not already in RGB
     if img.mode != 'RGB':
         img = ImageOps.grayscale(img).convert('RGB')
     
-    # Resize the image to match the input size of the model
+    # Resize the image
     img = img.resize((128, 128))
     
     # Convert image to array and normalize
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.0  # Normalize
+    img_array /= 255.0
     
     # Make the prediction
     prediction = model.predict(img_array)
@@ -50,15 +48,14 @@ def predict_image(img):
 # Streamlit app layout
 st.title('Eco-Bin')
 
-# File uploader for the image
 uploaded_file = st.file_uploader("Upload an image of the material", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
-    # Display the uploaded image
+  
     uploaded_img = Image.open(uploaded_file)
     st.image(uploaded_img, caption='Uploaded Image', use_container_width=True)
     
-    # Make prediction when the user clicks the button
+    # Make prediction
     if st.button('Predict'):
         with st.spinner('Classifying...'):
             prediction = predict_image(uploaded_img)
