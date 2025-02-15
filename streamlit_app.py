6d529e2783e8105fca5d72ab2601ef4e300ae25c
Sukruth S <100787053+I-Sukruth@users.noreply.github.com>
2025-02-15 11:02:01 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,24 @@ import streamlit as st
 from keras.models import load_model
 from keras.preprocessing import image
 import numpy as np
-import gdown  # To download from Google Drive
+import gdown 
 from PIL import Image
 
-# Download the model from Google Drive
 @st.cache_resource
 def load_model_from_drive():
-    url = 'https://drive.google.com/file/d/1ucpbyJY61OnCSRKbrj4FgYBSXX8FSONy/view?usp=sharing' 
+    url = 'https://drive.google.com/uc?id=1ucpbyJY61OnCSRKbrj4FgYBSXX8FSONy'
     output = 'Eco_Bin.h5'
+    
+    # Download the model file
     gdown.download(url, output, quiet=False)
+    
+    # Load the model
     return load_model(output)
 
 # Load the model
 model = load_model_from_drive()
 
-# Define the class labels (according to your folder structure)
+# Define the class labels
 class_labels = [
     'battery', 'biological', 'brown-glass', 'cardboard', 'clothes',
     'green-glass', 'metal', 'paper', 'plastic', 'shoes', 'trash', 'white-glass'
@@ -24,12 +27,16 @@ class_labels = [
 
 # Function to make predictions
 def predict_image(img):
-    img = img.resize((128, 128))  # Resize the image to match the input size of the model
+    img = img.resize((128, 128)) 
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array /= 255.0  # Normalize
+    
+    # Make the prediction
     prediction = model.predict(img_array)
     predicted_class_index = np.argmax(prediction, axis=1)[0]
+    
+    # Return the predicted class
     return class_labels[predicted_class_index]
 
 # Streamlit app layout
@@ -41,8 +48,8 @@ uploaded_file = st.file_uploader("Upload an image of the material", type=["jpg",
 if uploaded_file is not None:
     # Display the uploaded image
     uploaded_img = Image.open(uploaded_file)
-    st.image(uploaded_img, caption='Uploaded Image', use_column_width=True)
-
+    st.image(uploaded_img, caption='Uploaded Image', use_container_width=True) 
+    
     # Make prediction when the user clicks the button
     if st.button('Predict'):
         with st.spinner('Classifying...'):
